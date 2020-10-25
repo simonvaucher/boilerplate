18ea86fbddaf08f62728eb542f3546c29fc4195f
Simon Vaucher <s.vaucher@applicaster.com>
2020-10-25 15:30:56 +0200

    GRMBL wrong env var name

--- server/sequelize.js
+++ server/sequelize.js
@@ -5,7 +5,7 @@ const UserModel = require("./models/user");
 const BlogModel = require("./models/blog");
 const TagModel = require("./models/tag");
 
-const sequelize = new Sequelize(process.env.BOILDERPLATE_CONN_STRING, {
+const sequelize = new Sequelize(process.env.BOILERPLATE_CONN_STRING, {
   dialect: "mysql",
   pool: {
     max: 10,
